32059d211417c737454ae217eb5782642ad7a040
Your Name <gauravcollege09@gmail.com>
2025-07-17 22:43:50 +0530

    Update sitemap with lastmod and priority tags

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -3,32 +3,32 @@
 export async function GET() {
   const baseUrl = 'https://quickemi.site';
 
-  const staticPages = [
-    '', // Home page
-    '/about',
-    '/contact',
-    '/blog',
-    '/personal-loan',
-    '/car-loan',
-    '/home-loan',
-    '/blog/how-to-calculate-emi',
-    '/blog/personalvshome',
-    '/blog/blog3'
+  const pages = [
+    { path: '', lastmod: '2025-07-17', priority: '1.0' },             // Home page
+    { path: '/about', lastmod: '2025-07-10', priority: '0.5' },
+    { path: '/contact', lastmod: '2025-07-12', priority: '0.5' },
+    { path: '/blog', lastmod: '2025-07-14', priority: '0.6' },
+    { path: '/personal-loan', lastmod: '2025-07-15', priority: '0.9' },
+    { path: '/car-loan', lastmod: '2025-07-15', priority: '0.9' },
+    { path: '/home-loan', lastmod: '2025-07-15', priority: '0.9' },
+    { path: '/blog/how-to-calculate-emi', lastmod: '2025-07-13', priority: '0.7' },
+    { path: '/blog/personalvshome', lastmod: '2025-07-14', priority: '0.7' },
+    { path: '/blog/blog3', lastmod: '2025-07-11', priority: '0.6' }
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${staticPages
-      .map((page) => {
-        return `
+    ${pages.map((page) => {
+      return `
         <url>
-          <loc>${baseUrl}${page}</loc>
+          <loc>${baseUrl}${page.path}</loc>
+          <lastmod>${page.lastmod}</lastmod>
+          <priority>${page.priority}</priority>
         </url>`;
-      })
-      .join('')}
+    }).join('')}
   </urlset>`;
 
-  return new Response(sitemap, {
+  return new Response(sitemap.trim(), {
     headers: {
       'Content-Type': 'application/xml',
     },
